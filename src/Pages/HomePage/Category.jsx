6596b46ce62389f27edc81fee5fd62e57b0cdc65
najmul905 <nh4808563@gmail.com>
2024-02-24 21:42:39 +0600

    some change in offer section and category section

--- src/Pages/HomePage/Category.jsx
+++ src/Pages/HomePage/Category.jsx
@@ -1,45 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import './SliderStyle.css'
 const Category = () => {
-
-const [categories,setCategories]=useState([])
-
-useEffect(()=>{
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
     fetch('Categories.json')
-    .then(res=>res.json())
-    .then(data=>setCategories(data))
-},[])
-// console.log(categories)
-    return (
-        <div className='my-6 px-5'>
-            <h1 className='text-2xl font-semibold text-slate-500'>Categories</h1>
-            <div className='grid lg:grid-cols-5 md:grid-cols-4 grid-cols-3 gap-3 '>
-                {
-                    categories.map(category=><div key={category._id}>
-                        <motion.div
-                        
-                        className='border border-spacing-2 rounded shadow-2xl   my-3'>
-                            <header>
-                            <img className='rounded' src={category.image} alt="" />
-                            </header>
-                      <div className='p-2'>
-                      <div className='flex items-center justify-between mt-3'>
-                        <h1 className='text-[15px] '>{category.Category}</h1>
-                        <p className='text-[15px] '>{category.Name}</p>
-                       </div>
-                        < hr className='w-[100%] mx-auto my-2 border-stone-950' />
-                        <div>
-                        <h1 className='text-[15px]'>Price: <span className='text-red-600'>${category.price}</span></h1>
-                        <p className='text-sm '>{category.description}</p>
-                        </div>
-                      </div>
-                        </motion.div>
-                       
-                    </div>)
-                }
-            </div>
-        </div>
-    );
+      .then(res => res.json())
+      .then(data => setCategories(data))
+  }, [])
+  // console.log(categories)
+  return (
+    <div className='my-6 px-5'>
+      <h1 className='text-2xl font-semibold text-slate-500'>Categories</h1>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={5}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 6,
+            spaceBetween: 10,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper" >
+        {
+          categories.map(category => <SwiperSlide key={category._id}>
+            <motion.div
+              className='border  rounded hover:shadow-2xl cursor-pointer  my-3'>
+              <header>
+                <img className='rounded' src={category.image} alt="" />
+              </header>
+              <div className='p-2'>
+                <h1 className='text-[15px] text-center font-semibold'>{category.Category}</h1>
+              </div>
+            </motion.div>
+          </SwiperSlide>) }
+      </Swiper>
+    </div>
+  );
 };
 
 export default Category;
--- src/Pages/HomePage/Offer.jsx
+++ src/Pages/HomePage/Offer.jsx
@@ -1,32 +1,39 @@
 import React, { useEffect, useState } from 'react';
 
 const Offer = () => {
-    const [offers,setOffers]=useState([])
-    useEffect(()=>{
+    const [offers, setOffers] = useState([])
+    useEffect(() => {
         fetch('Discount.json')
-        .then(res=>res.json())
-        .then(data=>setOffers(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setOffers(data))
+    }, [])
     console.log(offers)
     return (
         <div className='my-6 px-5'>
             <h1 className='text-2xl font-semibold text-slate-500'>Offer</h1>
-            <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+            <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2'>
                 {
-                    offers.map(offer=>
-                     <div className='my-3 border border-spacing-2 rounded shadow-2xl relative' key={offer._Id}>
-                        <header>
-                            <img src={offer.Image} alt="" />
-                        </header>
-                        <div className='p-2'>
-                            <h1>Name: {offer.Name}</h1>
-                            <h1>Selling Price: <span className='line-through'>{offer.Price}</span></h1>
-                            <h1 className='absolute top-0 right-0 bg-black text-white m-1 px-2 rounded'>Offer: {offer.DiscountPercentage}%</h1>
-                            <hr className='border-black' />
-                            <h1>Discount Price: {(offer.Price-offer.Price*offer.DiscountPercentage/100).toFixed(2) }</h1>
-                        </div>
+                    offers.map(offer =>
+                        <div className={`my-3 border  rounded hover:shadow-2xl cursor-pointer relative`} key={offer._Id}>
+                            <header>
+                                <img src={offer.Image} alt="" />
+                            </header>
+                            <div className='p-2 truncate'>
+                                <h1 className='text-[12px] md:text-[1px]'>Name: {offer.Name}</h1>
+                                <div className='flex items-center'>
+                                    <p className='md:text-2xl font-semibold mr-2'>Price:</p>
+                                    <div>
+                                        <h1 className='line-through text-[12px] md:text-[16px]'>${offer.Price}</h1>
+                                        <h1 className='text-red-600 md:font-semibold'>${(offer.Price - offer.Price * offer.DiscountPercentage / 100).toFixed(2)}</h1>
+                                    </div>
+                                </div>
 
-                    </div>)
+                                <h1 className='absolute top-0 right-0 bg-black text-white m-1 px-2 rounded text-[10px] md:text-[12px] lg:text-[15px]'>Offer: {offer.DiscountPercentage}%</h1>
+                                <hr className='border-black' />
+                                <h1 className='truncate text-[12px] md:text-[16px]'>{offer.Description}</h1>
+                            </div>
+
+                        </div>)
                 }
             </div>
         </div>
